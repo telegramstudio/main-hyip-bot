17c9a5e07cc175514d862aa7bf6b08013e04a931
telegramstudio <reload.benjamin@gmail.com>
2017-12-19 01:25:59 +1000

    initialize new bot

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'telegram-bot-ruby'
+gem 'activerecord'
+gem 'i18n'
+gem 'pg'
+gem 'rake'
---
+++ lib/message_responder.rb
@@ -0,0 +1,53 @@
+require './models/user'
+require './lib/message_sender'
+
+class MessageResponder
+  attr_reader :message
+  attr_reader :bot
+  attr_reader :user
+
+  def initialize(options)
+    @bot = options[:bot]
+    @message = options[:message]
+    @user = User.find_or_create_by(uid: message.from.id)
+  end
+
+  def respond
+    on /^\/start/ do
+      answer_with_greeting_message
+    end
+
+    on /^\/stop/ do
+      answer_with_farewell_message
+    end
+  end
+
+  private
+
+  def on regex, &block
+    regex =~ message.text
+
+    if $~
+      case block.arity
+      when 0
+        yield
+      when 1
+        yield $1
+      when 2
+        yield $1, $2
+      end
+    end
+  end
+
+  def answer_with_greeting_message
+    answer_with_message I18n.t('greeting_message')
+  end
+
+  def answer_with_farewell_message
+    answer_with_message I18n.t('farewell_message')
+  end
+
+  def answer_with_message(text)
+    MessageSender.new(bot: bot, chat: message.chat, text: text).send
+  end
+end
