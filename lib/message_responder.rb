72f63036829b5a200522e547f119a8b7cf62d460
telegramstudio <reload.benjamin@gmail.com>
2017-12-19 03:20:46 +1000

    create main menu

--- lib/message_responder.rb
+++ lib/message_responder.rb
@@ -20,6 +20,23 @@ class MessageResponder
     on /^\/stop/ do
       answer_with_farewell_message
     end
+
+    on /^\Кошелек / do
+      wallet
+    end
+
+    on /^\Депозиты/ do
+      deposit 
+    end
+
+    on /^\Партнеры/ do
+      #partners
+    end
+
+    on /^\О сервисе/ do
+      #about
+    end
+
   end
 
   private
@@ -41,13 +58,36 @@ class MessageResponder
 
   def answer_with_greeting_message
     answer_with_message I18n.t('greeting_message')
-  end
+ end
 
   def answer_with_farewell_message
     answer_with_message I18n.t('farewell_message')
   end
 
   def answer_with_message(text)
-    MessageSender.new(bot: bot, chat: message.chat, text: text).send
+    answers = ["Кошелек", "Депозиты", "Партнеры", "О сервисе"]
+    MessageSender.new(bot: bot, chat: message.chat, text: text, answers: answers).send
   end
+
+  def wallet
+    text = "Ваш баланс: 0 BTC  ------------------------------" 
+    
+    kb = [
+          [Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Пополнить', callback_data: 'touch'),
+	    Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Вывести', callback_data: 'touch'),
+	  ],[Telegram::Bot::Types::InlineKeyboardButton.new(text: 'История транзакций', callback_data: 'touch')]]
+    
+    markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb, resize_keyboard: true)
+    
+    bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: markup)
+  end
+
 end
+
+
+
+
+
+
+
+
--- lib/reply_markup_formatter.rb
+++ lib/reply_markup_formatter.rb
@@ -7,6 +7,6 @@ class ReplyMarkupFormatter
 
   def get_markup
     Telegram::Bot::Types::ReplyKeyboardMarkup
-      .new(keyboard: array.each_slice(1).to_a, one_time_keyboard: true)
+      .new(keyboard: array.each_slice(2).to_a, one_time_keyboard: false, resize_keyboard: true)
   end
 end
