3462a61196060d16f6901969a8f597ebea9f62ff
telegramstudio <reload.benjamin@gmail.com>
2017-12-19 15:02:52 +1000

    add inline keyboard

--- lib/message_responder.rb
+++ lib/message_responder.rb
@@ -12,6 +12,16 @@ class MessageResponder
     @user = User.find_or_create_by(uid: message.from.id)
   end
 
+  def callback
+    if message.data == 'add_money_call'
+       bot.api.send_message(chat_id: message.from.id, text: "Пополнение счета")
+    elsif message.data == 'draw_money_call'
+      bot.api.send_message(chat_id: message.from.id, text: "Вывод средств")
+    elsif message.data == 'history_money'
+      bot.api.send_message(chat_id: message.from.id, text: "История транзакций")
+    end
+  end
+
   def respond
     on /^\/start/ do
       answer_with_greeting_message
@@ -21,20 +31,20 @@ class MessageResponder
       answer_with_farewell_message
     end
 
-    on /^\Кошелек / do
-      wallet
+    on /^\Кошелек/ do
+      answer_wallet
     end
 
     on /^\Депозиты/ do
-      deposit 
+      answer_deposit
     end
 
     on /^\Партнеры/ do
-      #partners
+      coming_soon
     end
 
     on /^\О сервисе/ do
-      #about
+      coming_soon
     end
 
   end
@@ -69,18 +79,35 @@ class MessageResponder
     MessageSender.new(bot: bot, chat: message.chat, text: text, answers: answers).send
   end
 
-  def wallet
+  def answer_wallet
     text = "Ваш баланс: 0 BTC  ------------------------------" 
     
     kb = [
-          [Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Пополнить', callback_data: 'touch'),
-	    Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Вывести', callback_data: 'touch'),
-	  ],[Telegram::Bot::Types::InlineKeyboardButton.new(text: 'История транзакций', callback_data: 'touch')]]
-    
+          [Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Пополнить', callback_data: 'add_money_call'),
+	    Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Вывести', callback_data: 'draw_money_call'),
+	  ],[Telegram::Bot::Types::InlineKeyboardButton.new(text: 'История транзакций', callback_data: 'history_money')]]
+     
     markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb, resize_keyboard: true)
     
     bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: markup)
   end
+
+  def answer_deposit
+   text = "Ваши депозиты: 0 BTC ----------------------------"
+   kb = [
+         [Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Внести',  callback_data: 'add_deposit_call'),
+          Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Текущие', callback_data: 'my_deposits_call')
+         ],
+         [Telegram::Bot::Types::InlineKeyboardButton.new(text: 'История депозитов', callback_data: 'history_deposits')]]
+
+   markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb, resize_keyboard: true) 
+   bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: markup)
+  end
+
+  def coming_soon
+    bot.api.send_message(chat_id: message.chat.id, text: "Недоступно в демо режиме")
+  end
+  
 
 end
 
